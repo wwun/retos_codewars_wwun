66973635d56fbba4b947f0b44930eaae7cb43b88
William Wun <williamwun@gmail.com>
2024-01-04 14:20:50 -0800

    Playing with digits

--- src/packagePrincial/ClasePrincipal.java
+++ src/packagePrincial/ClasePrincipal.java
@@ -57,10 +57,43 @@ public class ClasePrincipal {
         return str.replaceAll(".(?=.{4})", "#");
     }
 	*/
-	
+
+	/*best practice
+	public static long digPow(int n, int p) {
+    	String intString = String.valueOf(n);
+    	long sum = 0;
+    	for (int i = 0; i < intString.length(); ++i, ++p)
+      		sum += Math.pow(Character.getNumericValue(intString.charAt(i)), p);
+    	return (sum % n == 0) ? sum / n : -1;
+	}
+	 */
+
+	/*digPow
+	public static long digPow(int n, int p) {
+		long powerValue = powerValue(n, p);
+		if((((int)(powerValue/n))*n)==powerValue)
+			return (int)powerValue/n;
+		return -1;
+	}
+
+	public static long powerValue(int n, int p){
+		if(n<10){
+			return (long)Math.pow(n,p);
+		}
+		int sizeNumber = (int)Integer.toString(n).length();
+		int newN = (int)Math.pow((n%10),(p+sizeNumber-1));
+		return newN+powerValue((n/10), p);
+	}
+
+	//test
+	//powerValue(89,1);	//1
+	//powerValue(92,1);	//-1
+	//powerValue(46288,3)	//51
+	//powerValue(123,8)	//-1
+	*/
+
 	public static void main(String[] args) {
-		//System.out.println(spinWords("Welcome"));
-		System.out.println(maskify("William Wun"));		
+		
 	}
 
 }
